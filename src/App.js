8eaaa962f549b01aa7b2fe7385037ac05c834c6c
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-03-19 21:45:59 +0100

    Adding Meal component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
