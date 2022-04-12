8e52ee9d4f62f9ec2f0c956f004c2a77ebed25e8
Ouaklim Othmane <ouaklimothmane@gmail.com>
2022-04-12 01:37:26 +0000

    fix cart style

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -7,21 +7,23 @@ const Checkout = (props) => {
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
-      <div className={classes.control}>
-        <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
-      </div>
-      <div className={classes.control}>
-        <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+      <div className={classes.controls}>
+        <div className={classes.control}>
+          <label htmlFor="name">Your Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="street">Street</label>
+          <input type="text" id="street" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="postal">Postal Code</label>
+          <input type="text" id="postal" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="city">City</label>
+          <input type="text" id="city" />
+        </div>
       </div>
       <div className={classes.actions}>
         <button type="button" onClick={props.onCancel}>
